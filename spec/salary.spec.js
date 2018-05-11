e9579db81e62035f8cfacba099dbe85eccff5953
Matthew Knowles <matthewyknowles@gmail.com>
2018-05-11 00:08:06 -0400

    changed some variable names

--- spec/salary.spec.js
+++ spec/salary.spec.js
@@ -1,12 +1,12 @@
 describe("Salary Tests", ()=> {
   it("should verify susan's salary was increased", ()=> {
-    susan = {
+    employee = {
         salary : 50000,
         taxWithheld: 10000
     }
 
-    giveRaise(susan, 2000);
+    giveRaise(employee, 2000);
 
-    expect(susan.salary).toEqual(52000)
+    expect(employee.salary).toEqual(52000)
   })
 })
