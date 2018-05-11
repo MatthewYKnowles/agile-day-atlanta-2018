fb33e6efb0ddb87bb4f13bdedb86b27c44be1d76
Matthew Knowles <matthewyknowles@gmail.com>
2018-05-10 23:48:31 -0400

    first commit

---
+++ stryker.conf.js
@@ -0,0 +1,11 @@
+module.exports = function (config) {
+    config.set({
+    testRunner: 'karma',
+    testFramework: 'jasmine',
+    coverageAnalysis: 'perTest',
+    reporter: ['html', 'progress'],
+    karmaConfigFile: 'karma.conf.js',
+    mutate: ['src/**/*.js']
+    });
+    }
+    
